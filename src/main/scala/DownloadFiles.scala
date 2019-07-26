48e8685b409448c0ef77cd1977b5b73a5461d1e1
Suzanne Hamilton <suzanne.hamilton@nationalarchives.gov.uk>
2019-07-26 16:16:52 +0100

    List all objects in a folder in S3

--- src/main/scala/DownloadFiles.scala
+++ src/main/scala/DownloadFiles.scala
@@ -1,14 +1,33 @@
 import java.nio.file.{Path, Paths}
+import java.util.function.Consumer
 
 import software.amazon.awssdk.services.s3.S3Client
-import software.amazon.awssdk.services.s3.model.GetObjectRequest
+import software.amazon.awssdk.services.s3.model.{GetObjectRequest, ListObjectsV2Request}
+import software.amazon.awssdk.services.s3.paginators.ListObjectsV2Iterable
 
 object DownloadFiles extends App {
 
   val client = S3Client.create
-  val objectRequest = GetObjectRequest.builder.bucket("tdr-files").key("tmp-play-app/MyPlainTextCat.txt").build
-  val outputPath: Path = Paths.get("/tmp/tdr-export/some-cat.txt")
-  val response = client.getObject(objectRequest, outputPath)
 
-  println(response.lastModified)
+  val consumer: Consumer[ListObjectsV2Request.Builder] = (requestBuilder: ListObjectsV2Request.Builder) => {
+    requestBuilder
+      .bucket("tdr-files")
+      .prefix("tmp-play-app")
+  }
+  val response: ListObjectsV2Iterable = client.listObjectsV2Paginator(consumer)
+
+  println("S3 objects:")
+  response.contents.stream.forEach(s3Object => {
+    // Includes folder
+    // Includes key prefix
+    println(s3Object.key())
+  })
+
+
+
+//  val objectRequest = GetObjectRequest.builder.bucket("tdr-files").key("tmp-play-app/MyPlainTextCat.txt").build
+//  val outputPath: Path = Paths.get("/tmp/tdr-export/some-cat.txt")
+//  val response = client.getObject(objectRequest, outputPath)
+//
+//  println(response.lastModified)
 }
