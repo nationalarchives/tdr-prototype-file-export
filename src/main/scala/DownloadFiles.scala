efb8e4bb44d4191af2ee31b89abf38fa54540d01
Suzanne Hamilton <suzanne.hamilton@nationalarchives.gov.uk>
2019-07-26 16:56:08 +0100

    Download every non-empty file in a bucket
    Skip empty files because they cause a download exception. In TDR,
    we should have rejected all empty files before the transfer is
    exported so we should report an error here.

--- src/main/scala/DownloadFiles.scala
+++ src/main/scala/DownloadFiles.scala
@@ -1,33 +1,49 @@
-import java.nio.file.{Path, Paths}
+import java.io.File
+import java.nio.file.{Files, Path, Paths}
 import java.util.function.Consumer
 
 import software.amazon.awssdk.services.s3.S3Client
-import software.amazon.awssdk.services.s3.model.{GetObjectRequest, ListObjectsV2Request}
+import software.amazon.awssdk.services.s3.model.{GetObjectRequest, ListObjectsV2Request, S3Object}
 import software.amazon.awssdk.services.s3.paginators.ListObjectsV2Iterable
 
 object DownloadFiles extends App {
 
   val client = S3Client.create
 
-//  val consumer: Consumer[ListObjectsV2Request.Builder] = (requestBuilder: ListObjectsV2Request.Builder) => {
-//    requestBuilder
-//      .bucket("tdr-files")
-//      .prefix("tmp-play-app")
-//  }
-//  val response: ListObjectsV2Iterable = client.listObjectsV2Paginator(consumer)
-//
-//  println("S3 objects:")
-//  response.contents.stream.forEach(s3Object => {
-//    // Includes folder
-//    // Includes key prefix
-//    println(s3Object.key())
-//  })
+  val consumer: Consumer[ListObjectsV2Request.Builder] = (requestBuilder: ListObjectsV2Request.Builder) => {
+    requestBuilder
+      .bucket("tdr-files")
+      .prefix("tmp-play-app")
+  }
+  val response: ListObjectsV2Iterable = client.listObjectsV2Paginator(consumer)
 
+  val tempFolder = Files.createTempDirectory("tdr-export")
+  println(s"Saving files to ${tempFolder.toAbsolutePath}")
 
+  println("S3 objects:")
+  response.contents.stream.forEach(s3Object => {
+    if (s3Object.key.endsWith("/")) {
+      println(s"Creating directory '${s3Object.key}'")
+      createDirectory(s3Object.key, tempFolder)
+    } else if (s3Object.size == 0) {
+      println(s"Skipping '${s3Object.key}' because size is zero")
+    } else {
+      println(s"Downloading '${s3Object.key}'")
+      download(s3Object, tempFolder.toAbsolutePath)
+    }
+  })
 
-  val objectRequest = GetObjectRequest.builder.bucket("tdr-files").key("tmp-play-app/some-nested-folder/").build
-  val outputPath: Path = Paths.get("/tmp/tdr-export/some-nested-folder/")
-  val response = client.getObject(objectRequest, outputPath)
+  private def download(s3Object: S3Object, tempFolder: Path): Unit = {
+    val objectRequest = GetObjectRequest.builder
+      .bucket("tdr-files")
+      .key(s3Object.key)
+      .build
+    val outputPath: Path = Paths.get(s"$tempFolder/${s3Object.key}")
+    client.getObject(objectRequest, outputPath)
+  }
 
-  println(response.lastModified)
+  private def createDirectory(directoryPath: String, tempFolder: Path): Unit = {
+    val absolutePath = Paths.get(s"${tempFolder.toAbsolutePath}/$directoryPath")
+    new File(absolutePath.toString).mkdirs
+  }
 }
