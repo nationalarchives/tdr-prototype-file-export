8795176d3e8bb9a8bd618a76b92a80e31f98e592
Suzanne Hamilton <suzanne.hamilton@nationalarchives.gov.uk>
2019-07-30 12:02:36 +0100

    Upload a hard-coded tar.gz file to S3

--- export-zip/src/main/scala/ExportZip.scala
+++ export-zip/src/main/scala/ExportZip.scala
@@ -1,3 +1,25 @@
+import java.nio.file.Paths
+import java.util.UUID
+import java.util.function.Consumer
+
+import software.amazon.awssdk.services.s3.S3Client
+import software.amazon.awssdk.services.s3.model.PutObjectRequest
+
 object ExportZip extends App {
   println("In export zip app")
+
+  val archiveFilename = "/tmp/tdr-files.tar.gz"
+  val archivePath = Paths.get(archiveFilename)
+
+  val client = S3Client.create
+
+  val objectKey = s"transfer-${UUID.randomUUID}.tar.gz"
+
+  val consumer: Consumer[PutObjectRequest.Builder] = (requestBuilder: PutObjectRequest.Builder) => {
+    requestBuilder
+      .bucket("tdr-prototype-export")
+      .key(objectKey)
+  }
+
+  client.putObject(consumer, archivePath)
 }
